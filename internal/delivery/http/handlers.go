1ddb6116819ff0b20e1c0ec67d28896f421d59a0
Pavel Korchagin <mortalis94@gmail.com>
2023-02-03 19:30:19 +0300

    Fix cors: added Authorization header to allowed

--- internal/delivery/http/handlers.go
+++ internal/delivery/http/handlers.go
@@ -153,7 +153,7 @@ func Init(container service.ServiceContainer, cfg *config.Config) http.Handler {
 			http.MethodHead, http.MethodGet, http.MethodPost,
 			http.MethodPut, http.MethodPatch, http.MethodDelete,
 		},
-		AllowedHeaders:   []string{"Content-Type", "X-Fingerprint"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization", "X-Fingerprint"},
 		MaxAge:           cfg.Cors.MaxAge,
 		AllowCredentials: true,
 		Debug:            cfg.IsDebug,
