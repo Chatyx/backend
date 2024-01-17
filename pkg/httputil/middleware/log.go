0b6ca49db41b945ca1cf90ffb424031c8ae8a11c
Pavel Korchagin <mortalis94@gmail.com>
2024-01-17 17:21:02 +0300

    chore: implement log middleware

---
+++ pkg/httputil/middleware/log.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/Chatyx/backend/pkg/log"
+)
+
+type statusCodeRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (rec *statusCodeRecorder) WriteHeader(statusCode int) {
+	rec.StatusCode = statusCode
+	rec.ResponseWriter.WriteHeader(statusCode)
+}
+
+func Log(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		rec := &statusCodeRecorder{ResponseWriter: w}
+
+		begin := time.Now()
+		next.ServeHTTP(rec, req)
+
+		logger := log.FromContext(req.Context())
+		logger.With(
+			"path", req.URL.EscapedPath(),
+			"user_agent", req.UserAgent(),
+			"method", req.Method,
+			"ip", req.RemoteAddr,
+			"host", req.Host,
+			"request_size", req.ContentLength,
+			"duration", time.Since(begin),
+			"status_code", rec.StatusCode,
+		).Info("Request handled")
+	})
+}
