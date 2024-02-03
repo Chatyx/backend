c430d73da0d2eb238d0e8abc2ca2ada01aad9881
Pavel Korchagin <mortalis94@gmail.com>
2024-02-03 15:47:56 +0300

    chore: set indices

---
+++ db/migrations/20240203112038_set_indices.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS dialog_participants__user_id__idx;
+
+DROP INDEX IF EXISTS group_participants__user_id__idx;
+
+DROP INDEX IF EXISTS messages__chat_id_chat_type__idx;
+
+DROP INDEX IF EXISTS messages__sent_at__idx;
